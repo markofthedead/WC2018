4dd2d2850ebe7e776e9498a172ae4189f5efe864
Mark Wallace <markwallace@marks-mbp-3.home>
2018-05-15 21:51:58 +0100

    mobile changes

---
+++ _www/WC2018/js/script-precompile.js
@@ -0,0 +1,11 @@
+toggleMenu = function(){
+  const header = document.getElementsByTagName('header')[0];
+  header.classList.toggle("closed");
+}
+
+document.getElementById("menuButton").addEventListener("click", function(e){
+  e.preventDefault();
+  toggleMenu();
+});
+
+toggleMenu();
